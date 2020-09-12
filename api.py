e83ae43126de6d5f1ba222be1bfb84d0d0751fd9
Yodart <joaogabrielgcoutinho@gmail.com>
2020-09-12 20:13:39 -0300

    Added User Service Blueprint to the Api

--- api.py
+++ api.py
@@ -8,9 +8,11 @@ from db import Database
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
 from auth_service import auth_service, require_auth_token
+from user_service import user_service
 
 app = Flask(__name__)
 app.register_blueprint(auth_service)
+app.register_blueprint(user_service)
 db = Database("todoodledb")
 
 
@@ -41,31 +43,6 @@ def delete_single_todo(todo_id):
 @ app.route('/users', methods=['GET'])
 def get_users():
     return db.get_users(limit=request.args.get('limit'), offset=request.args.get('offset'))
-
-
-@ app.route('/user', methods=['POST'])
-def create_user():
-    hashed_password = generate_password_hash(
-        request.json['password'], method='sha256')
-    return db.create_user(name=request.json['name'], password=hashed_password)
-
-
-@ app.route('/user/<int:user_id>', methods=['GET'])
-def get_single_user_by_id(user_id):
-    return db.get_single_user_by_id([user_id])
-
-
-@ app.route('/user/<int:user_id>', methods=['PUT'])
-def edit_single_user(user_id):
-    name = request.json['name']if 'name' in request.json else None
-    password = generate_password_hash(
-        request.json['password'], method='sha256') if 'password' in request.json else None
-    return db.edit_user(user_id, name, password)
-
-
-@ app.route('/user/<int:user_id>', methods=['DELETE'])
-def delete_single_user(user_id):
-    return db.delete_user([user_id])
 
 
 if __name__ == '__main__':
